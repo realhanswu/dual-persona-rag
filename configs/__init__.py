cf61dad551e58911ed05e4685571c1d6923c0da6
Nan Wu <53102280+realhanswu@users.noreply.github.com>
2026-05-06 15:25:27 -0700

    Initialize configs module with imports and exports

---
+++ configs/__init__.py
@@ -0,0 +1,7 @@
+from configs.presets import get_preset, list_presets, PRESETS
+from configs.personas import get_thresholds, get_composite_weights, get_pas_sub_dims
+
+__all__ = [
+    "get_preset", "list_presets", "PRESETS",
+    "get_thresholds", "get_composite_weights", "get_pas_sub_dims",
+]
